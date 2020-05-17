0a4e9ec35b070ed4324bf2ed44fb3b9c61871ef7
Daniel Mudlaff <dmudlaff89@gmail.com>
2020-05-17 19:06:25 +0200

    Added draw particle function in new js file

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(window.innerWidth, window.innerHeight);
+} 
+function draw() {
+p = new Particle();
+}
+function draw() {
+    p.draw()
+}
+
+class Particle {
+    constructor() {
+        // Position
+        this.pos = createVector(random(width), random(height));
+        // Size
+        this.size = 10;
+    }
+    
+}
